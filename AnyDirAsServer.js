1c2c72bfd8b4958983bc55f816d8f1e88a4276ee
秦真 <andy@qinzhendeMacBook-Pro.local>
2018-04-20 00:29:42 +0800

    将电脑目录作为文件浏览器

---
+++ AnyDirAsServer.js
@@ -0,0 +1,52 @@
+/**
+ * 使用nodejs把电脑指定的文件夹目录当作web服务器来访问
+ * - 如果是文件，则显示文件内容
+ * - 如果是文件夹，则列出当前文件夹目录列表
+ */
+var http = require('http');
+var fs = require('fs');
+
+// 默认当前路径
+var defaultPath = __dirname;
+
+var server = http.createServer(function(req, res) {
+	// 访问 http://localhost:3000 时列出当前目录列表
+	console.log('visited : ', req.url);
+	if ('/' === req.url) {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		return res.end(fileList());
+	}
+	var filepath = decodeURI(req.url);
+	var exists = fs.existsSync(filepath);
+
+	if (exists) {
+		var stat = fs.lstatSync(filepath);
+		if (stat.isFile()) {
+			res.writeHead(200, { 'Content-Type': 'text' });
+			return fs.createReadStream(filepath).pipe(res);
+		} else if (stat.isDirectory()) {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			return res.end(fileList(filepath));
+		}
+	} else {
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+		return res.end('<h2>没有找到文件</h2');
+	}
+
+	/**
+	 * 列出指定文件目录，默认当前目录
+	 */
+	function fileList(path = defaultPath) {
+		var html = '';
+		console.log(path);
+		fs.readdirSync(path).forEach(function(item) {
+			var href = (path + '/' + item).replace('//', '/');
+			html += '<a href="' + href + '">' + item + '</a><br/>';
+		});
+		return html;
+	}
+});
+
+server.listen(3000, function() {
+	console.log('Server started at http://localhost:3000');
+});
